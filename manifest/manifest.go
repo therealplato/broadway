70752e23210a49b96189c00682337d940cdd85fc
Attila Maczak <attila@maczak.hu>
2016-03-11 11:19:47 -0500

    fix typo + id attribute instead of name

--- manifest/manifest.go
+++ manifest/manifest.go
@@ -6,22 +6,23 @@ import (
 )
 
 // Manifest represents a kubernetes manifest file
+// Filename is used as identifier in the current implementation.
 type Manifest struct {
-	Name     string
+	Id       string
 	template *template.Template
 }
 
 // New creates a new Manifest object and parses the template
-func New(name, content string) (*Manifest, error) {
-	t, err := template.New(name).Parse(content)
+func New(id, content string) (*Manifest, error) {
+	t, err := template.New(id).Parse(content)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Manifest{Name: name, template: t}, nil
+	return &Manifest{Id: id, template: t}, nil
 }
 
-// Execute runs teamplate with variables
+// Execute executes template with variables
 func (m *Manifest) Execute(vars map[string]string) string {
 	var b bytes.Buffer
 	err := m.template.Execute(&b, vars)
