a334b00ae3acf247b83c8f289319be3c73658a99
therealplato <therealplato@gmail.com>
2016-03-11 15:46:35 -0500

    Attempt to parse everything in /playbooks on boot

--- main.go
+++ main.go
@@ -2,31 +2,49 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-
 	"github.com/namely/broadway/instance"
 	"github.com/namely/broadway/playbook"
+	"log"
+	"path/filepath"
 )
 
 func main() {
-	args := os.Args
-	yamlFileDescriptor := args[1:][0]
-
-	playbookBytes, err := playbook.ReadPlaybookFromDisk(yamlFileDescriptor)
+	/*
+		args := os.Args
+		yamlFileDescriptor := args[1:][0]
+	*/
+	playbooks, err := loadPlaybookFolder("playbooks/")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pb, err := playbook.ParsePlaybook(playbookBytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := pb.ValidateTasks(); err != nil {
-		log.Fatalf("Task validation failed: %s", err)
-	}
-
+	fmt.Printf("%v+\n", playbooks)
 	fmt.Println(instance.InstanceStatusNew)
-	fmt.Printf("%+v", pb)
+}
+
+func loadPlaybookFolder(dir string) ([]playbook.Playbook, error) {
+	var AllPlaybooks []playbook.Playbook
+	paths, err := filepath.Glob(dir + "/*")
+	if err != nil {
+		return AllPlaybooks, err
+	}
+	for _, path := range paths {
+		playbookBytes, err := playbook.ReadPlaybookFromDisk(path)
+		if err != nil {
+			fmt.Printf("Warning: Failed to read %s\n", path)
+			continue
+		}
+		parsed, err := playbook.ParsePlaybook(playbookBytes)
+		if err != nil {
+			fmt.Printf("Warning: Failed to parse %s\n", path)
+			continue
+		}
+		err = parsed.Validate()
+		if err != nil {
+			fmt.Printf("Warning: Playbook %s invalid: %s\n", path, err)
+			continue
+		}
+		AllPlaybooks = append(AllPlaybooks, parsed)
+	}
+	return AllPlaybooks, nil
 }
