86cf6267ab4eeb2369d80e7214af94b7b8a1dcbc
therealplato <therealplato@gmail.com>
2016-03-18 11:15:04 -0400

    Test GetStatus for default and all other Status values

--- instance/instance_test.go
+++ instance/instance_test.go
@@ -16,13 +16,54 @@ func TestGetStatusFailure(t *testing.T) {
 	assert.Equal(t, "Instance does not exist.", err.Error(), "wrong err: GetStatus with bad arguments")
 }
 
+func TestGetDefaultStatus(t *testing.T) {
+	s := store.New()
+	testcases := []struct {
+		attrs Attributes
+	}{
+		{
+			Attributes{PlaybookID: "test", ID: "present"},
+		},
+	}
+
+	for _, testcase := range testcases {
+		i := New(s, &testcase.attrs)
+		err := i.Save()
+		assert.Nil(t, err)
+
+		status, err := GetStatus(s, testcase.attrs.PlaybookID, testcase.attrs.ID)
+		assert.Nil(t, err, "err of GetStatus with good arguments should be nil")
+		assert.Equal(t, StatusNew, status, "GetStatus() of newly created instance should be NewStatus")
+	}
+}
+
 func TestGetStatusSuccess(t *testing.T) {
 	s := store.New()
-	i := New(s, &Attributes{PlaybookID: "test", ID: "present"})
-	err := i.Save()
-	assert.Nil(t, err)
 
-	status, err := GetStatus(s, "test", "present")
-	assert.Nil(t, err, "err of GetStatus with good arguments should be nil")
-	assert.Equal(t, StatusNew, status, "Status of GetStatus(new) should be StatusNew")
+	testcases := []struct {
+		attrs Attributes
+	}{
+		{
+			Attributes{PlaybookID: "test", ID: "present", Status: StatusDeployed},
+		},
+		{
+			Attributes{PlaybookID: "test", ID: "present", Status: StatusDeploying},
+		},
+		{
+			Attributes{PlaybookID: "test", ID: "present", Status: StatusError},
+		},
+		{
+			Attributes{PlaybookID: "test", ID: "present", Status: StatusDeleting},
+		},
+	}
+
+	for _, testcase := range testcases {
+		i := New(s, &testcase.attrs)
+		err := i.Save()
+		assert.Nil(t, err)
+
+		status, err := GetStatus(s, testcase.attrs.PlaybookID, testcase.attrs.ID)
+		assert.Nil(t, err, "err of GetStatus with good arguments should be nil")
+		assert.Equal(t, testcase.attrs.Status, status, "GetStatus returned unexpected Status")
+	}
 }
