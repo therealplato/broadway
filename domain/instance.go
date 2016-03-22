2db505a854a1c1616b362ebe525559945cf66219
Rafael George <george.rafael@gmail.com>
2016-03-22 09:26:30 -0400

    Add comments for the instance domain definitions

--- domain/instance.go
+++ domain/instance.go
@@ -6,6 +6,7 @@ import (
 	"github.com/namely/broadway/store"
 )
 
+// Instance entity
 type Instance struct {
 	PlaybookID string            `json:"playbook_id" binding:"required"`
 	ID         string            `json:"id"`
@@ -13,6 +14,7 @@ type Instance struct {
 	Vars       map[string]string `json:"vars"`
 }
 
+// Instance json representation
 func (i *Instance) JSON() (string, error) {
 	encoded, err := json.Marshal(i)
 	if err != nil {
@@ -21,22 +23,27 @@ func (i *Instance) JSON() (string, error) {
 	return string(encoded), nil
 }
 
+// Path for an instance
 func (i *Instance) Path() string {
 	return "/broadway/instances/" + i.PlaybookID + "/" + i.ID
 }
 
+// Instance repository interface
 type InstanceRepository interface {
 	Save(instance Instance) error
 }
 
+// Instance repository to handle persistence logic
 type InstanceRepo struct {
 	store store.Store
 }
 
+// Create a new instance repo
 func NewInstanceRepo(s store.Store) *InstanceRepo {
 	return &InstanceRepo{store: s}
 }
 
+// Save a new instance as json
 func (ir *InstanceRepo) Save(instance Instance) error {
 	encoded, err := instance.JSON()
 	if err != nil {
