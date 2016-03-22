57c5d56f695bbd6c977dd65547249bf0d8362c91
Rafael George <george.rafael@gmail.com>
2016-03-22 09:30:49 -0400

    Add comments for the instance service definition

--- services/instance.go
+++ services/instance.go
@@ -2,14 +2,17 @@ package services
 
 import "github.com/namely/broadway/domain"
 
+// InstanceService definition
 type InstanceService struct {
 	repo domain.InstanceRepository
 }
 
+// NewInstanceService creates a new instance service
 func NewInstanceService(r domain.InstanceRepository) *InstanceService {
 	return &InstanceService{repo: r}
 }
 
+// Create a new instance
 func (is *InstanceService) Create(i domain.Instance) error {
 	return is.repo.Save(i)
 }
