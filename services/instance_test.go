bdbd3472c760c0d1296a23b560840c3529eab9cf
Rafael George <george.rafael@gmail.com>
2016-03-23 11:22:40 -0400

    Add a happy path test for getting an status Remove instance/instance_test.go

--- services/instance_test.go
+++ services/instance_test.go
@@ -20,6 +20,27 @@ func TestCreateInstance(t *testing.T) {
 	assert.Equal(t, broadway.StatusNew, createdInstance.Status)
 }
 
+func TestGetStatus(t *testing.T) {
+	store := store.New()
+	service := NewInstanceService(store)
+
+	testcases := []broadway.Instance{
+		broadway.Instance{PlaybookID: "test", ID: "present", Status: broadway.StatusDeployed},
+		broadway.Instance{PlaybookID: "test", ID: "present", Status: broadway.StatusDeploying},
+		broadway.Instance{PlaybookID: "test", ID: "present", Status: broadway.StatusError},
+		broadway.Instance{PlaybookID: "test", ID: "present", Status: broadway.StatusDeleting},
+	}
+
+	for _, i := range testcases {
+		err := service.Create(i)
+		assert.Nil(t, err)
+
+		status, err := service.GetStatus(i.Path())
+		assert.Nil(t, err, "err of GetStatus with good arguments should be nil")
+		assert.Equal(t, i.Status, status, "GetStatus returned unexpected Status")
+	}
+}
+
 func TestGetStatusFailure(t *testing.T) {
 	store := store.New()
 	service := NewInstanceService(store)
@@ -29,7 +50,4 @@ func TestGetStatusFailure(t *testing.T) {
 	status, err := service.GetStatus(i.Path())
 	assert.NotNil(t, err)
 	assert.Equal(t, broadway.StatusNew, status, "status of GetStatus(missing) should be StatusNew")
-}
-
-func TestGetStatus(t *testing.T) {
 }
