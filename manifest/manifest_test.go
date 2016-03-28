67538440b64d560f7d1e4b17857e88501d05a957
Attila Maczak <attila@maczak.hu>
2016-03-28 11:05:41 -0400

    fix manifest test

--- manifest/manifest_test.go
+++ manifest/manifest_test.go
@@ -2,16 +2,13 @@ package manifest
 
 import (
 	"testing"
-	"text/template"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestExecute(t *testing.T) {
-	id := "test"
-	tp, err := template.New(id).Parse(`{{ .test }}`)
+	m, err := New("test", `{{ .test }}`)
 	assert.Nil(t, err)
-	m := &Manifest{ID: id, Template: tp}
 
 	out := m.Execute(map[string]string{"test": "hello!"})
 
