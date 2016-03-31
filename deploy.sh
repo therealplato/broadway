720679a128ac836966dde212e01c219402089bdb
Attila Maczak <attila@maczak.hu>
2016-03-31 17:28:48 -0400

    fix deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-rm broadway
-COMMIT=$(git rev-parse HEAD) | cut -c1-8
+COMMIT=$(git rev-parse HEAD | cut -c1-8)
 docker-compose run -e CGO_ENABLED=0 test go build -a -installsuffix cgo -ldflags "-s"
 docker build -t registry.namely.tech/namely/broadway:$COMMIT -f Dockerfile-build .
 docker push registry.namely.tech/namely/broadway:$COMMIT
