59d837f6604dfdc1150484829e8355646315b0a1
Attila Maczak <attila@maczak.hu>
2016-03-31 17:49:13 -0400

    attempt to fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 COMMIT=$(git rev-parse HEAD | cut -c1-8)
+echo "Version: $COMMIT"
 docker-compose run -e CGO_ENABLED=0 test go build -a -installsuffix cgo -ldflags "-s"
 docker build -t registry.namely.tech/namely/broadway:$COMMIT -f Dockerfile-build .
 docker push registry.namely.tech/namely/broadway:$COMMIT
