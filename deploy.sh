bc223b4c47bfa7c37d14316aab6916de52e41f4c
Attila Maczak <attila@maczak.hu>
2016-03-31 17:07:47 -0400

    get git hash

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 rm broadway
+COMMIT=$(git rev-parse HEAD) | cut -c1-8
 docker-compose run -e CGO_ENABLED=0 test go build -a -installsuffix cgo -ldflags "-s"
-docker build -t registry.namely.tech/namely/broadway:`echo $TRAVIS_COMMIT | cut -c1-8` -f Dockerfile-build .
-docker push registry.namely.tech/namely/broadway:`echo $TRAVIS_COMMIT | cut -c1-8`
+docker build -t registry.namely.tech/namely/broadway:$COMMIT -f Dockerfile-build .
+docker push registry.namely.tech/namely/broadway:$COMMIT
