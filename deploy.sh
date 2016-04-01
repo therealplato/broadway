cdf55212cf48305c73accf5af7a7795e9992d587
Attila Maczak <attila@maczak.hu>
2016-04-01 11:46:45 -0400

    use deploy script to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 COMMIT=$(git rev-parse HEAD | cut -c1-8)
-echo "Version: $COMMIT"
+docker login -e=$DOCKER_EMAIL -u=$DOCKER_USER -p=$DOCKER_PASSWORD
 docker-compose run -e CGO_ENABLED=0 test go build -a -installsuffix cgo -ldflags "-s"
 docker build -t registry.namely.tech/namely/broadway:$COMMIT -f Dockerfile-build .
 docker push registry.namely.tech/namely/broadway:$COMMIT
