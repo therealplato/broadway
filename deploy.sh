0370750526ab213b8beb5da45e7ad4e7e1b829e3
Attila Maczak <attila@maczak.hu>
2016-04-01 11:55:47 -0400

    add sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 COMMIT=$(git rev-parse HEAD | cut -c1-8)
 docker login -e=$DOCKER_EMAIL -u=$DOCKER_USER -p=$DOCKER_PASSWORD
 docker-compose run -e CGO_ENABLED=0 test go build -a -installsuffix cgo -ldflags "-s"
