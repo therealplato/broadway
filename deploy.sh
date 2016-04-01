ccd11391506dfe9f9611f36c46e4ed6576507694
Attila Maczak <attila@maczak.hu>
2016-04-01 12:09:02 -0400

    fix docker repo name

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 COMMIT=$(git rev-parse HEAD | cut -c1-8)
 docker login -e=$DOCKER_EMAIL -u=$DOCKER_USER -p=$DOCKER_PASSWORD
 docker-compose run -e CGO_ENABLED=0 test go build -a -installsuffix cgo -ldflags "-s"
-docker build -t registry.namely.tech/namely/broadway:$COMMIT -f Dockerfile-build .
-docker push registry.namely.tech/namely/broadway:$COMMIT
+docker build -t namely/broadway:$COMMIT -f Dockerfile-build .
+docker push namely/broadway:$COMMIT
