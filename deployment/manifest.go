d8d285f816a4b2ea652f4c49cf016e16e74f17fc
Attila Maczak <attila@maczak.hu>
2016-05-24 16:42:27 -0400

    Template functions in manifest templates (#84)
    * adding template functions to manifest templates
    
    * change label

--- deployment/manifest.go
+++ deployment/manifest.go
@@ -4,13 +4,26 @@ import (
 	"bytes"
 	"os"
 	"path/filepath"
+	"strings"
 	"text/template"
+	"time"
 
+	"github.com/golang/glog"
 	"github.com/namely/broadway/env"
 )
 
 // ManifestExtension is added to each task manifest item to make a filename
 var ManifestExtension = ".yml"
+
+var templateFuncs = template.FuncMap{
+	"split":    strings.Split,
+	"join":     strings.Join,
+	"datetime": time.Now,
+	"toUpper":  strings.ToUpper,
+	"toLower":  strings.ToLower,
+	"contains": strings.Contains,
+	"replace":  strings.Replace,
+}
 
 // Manifest represents a kubernetes manifest file
 // Filename is used as identifier in the current implementation.
@@ -21,7 +34,7 @@ type Manifest struct {
 
 // NewManifest creates a new Manifest object and parses (but does not execute) the template
 func NewManifest(id, content string) (*Manifest, error) {
-	t, err := template.New(id).Parse(content)
+	t, err := template.New(id).Funcs(templateFuncs).Parse(content)
 	if err != nil {
 		return nil, err
 	}
@@ -34,6 +47,7 @@ func (m *Manifest) Execute(vars map[string]string) string {
 	var b bytes.Buffer
 	err := m.Template.Execute(&b, vars)
 	if err != nil {
+		glog.Errorf("%s template errored, ignoring: %s", m.ID, err.Error())
 		return ""
 	}
 	return b.String()
--- deployment/manifest_test.go
+++ deployment/manifest_test.go
@@ -7,10 +7,30 @@ import (
 )
 
 func TestExecute(t *testing.T) {
-	m, err := NewManifest("test", `{{ .test }}`)
-	assert.Nil(t, err)
+	cases := []struct {
+		scenario string
+		template string
+		vars     map[string]string
+		expected string
+	}{
+		{
+			scenario: "simple",
+			template: `{{ .test }}`,
+			vars:     map[string]string{"test": "hello!"},
+			expected: "hello!",
+		},
+		{
+			scenario: "template",
+			template: `{{ $cities := split .test ","}}{{ join $cities "+" }}`,
+			vars:     map[string]string{"test": "brooklyn,manhattan"},
+			expected: "brooklyn+manhattan",
+		},
+	}
 
-	out := m.Execute(map[string]string{"test": "hello!"})
-
-	assert.Equal(t, "hello!", out)
+	for _, c := range cases {
+		m, err := NewManifest(c.scenario, c.template)
+		assert.Nil(t, err)
+		out := m.Execute(c.vars)
+		assert.Equal(t, c.expected, out, c.scenario+" case does not match output")
+	}
 }
