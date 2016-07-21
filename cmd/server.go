ce9ab18dfec9726f414883ff1f2f17df1622cddb
therealplato <therealplato@gmail.com>
2016-07-21 07:53:56 -0400

    declare auth-token, slack-token flags

--- cmd/server.go
+++ cmd/server.go
@@ -9,6 +9,9 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
+// ServerCfg is the configuration object for the server
+var ServerCfg cfg.BroadwayServer
+
 // ServerCmd is executed by cli on `broadway server`
 var ServerCmd = func(c *cli.Context) error {
 	fmt.Println("starting server...")
@@ -22,9 +25,6 @@ var ServerCmd = func(c *cli.Context) error {
 	return nil
 }
 
-// ServerCfg is the configuration object for the server
-var ServerCfg cfg.BroadwayServer
-
 // ServerCmdFlags declare what flags can be passed to the `server` subcommand
 var ServerCmdFlags = []cli.Flag{
 	cli.StringFlag{
@@ -33,5 +33,20 @@ var ServerCmdFlags = []cli.Flag{
 		Usage:       "listen address for broadway http api server",
 		EnvVar:      "HOST",
 		Destination: &ServerCfg.ServerHost,
+	},
+	cli.StringFlag{
+		Name:        "auth, auth-token",
+		Usage:       "a global bearer token required for http api requests", // but not GET/POST command/
+		EnvVar:      "BROADWAY_AUTH_TOKEN",
+		Destination: &ServerCfg.AuthBearerToken,
+	},
+	// slack-token is sent from Slack to POST command/ and can be found on Slack's
+	// Custom Command configuration page.
+	// broadway denies the request if it doesn't match this config value
+	cli.StringFlag{
+		Name:        "slack, slack-token",
+		Usage:       "the expected Slack custom command token",
+		EnvVar:      "BROADWAY_AUTH_TOKEN",
+		Destination: &ServerCfg.AuthBearerToken,
 	},
 }
