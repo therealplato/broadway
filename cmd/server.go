03b81ed992d02793216427a5a3c6e4709074d9b4
therealplato <therealplato@gmail.com>
2016-07-21 07:34:03 -0400

    parse host flag with cli and pass to server

--- cmd/server.go
+++ cmd/server.go
@@ -3,19 +3,35 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/namely/broadway/env"
+	"github.com/namely/broadway/cfg"
 	"github.com/namely/broadway/server"
 	"github.com/namely/broadway/store"
 	"gopkg.in/urfave/cli.v1"
 )
 
+// ServerCmd is executed by cli on `broadway server`
 var ServerCmd = func(c *cli.Context) error {
 	fmt.Println("starting server...")
 	s := server.New(store.New())
 	s.Init()
-	err := s.Run(env.ServerHost)
+	// err := s.Run(env.ServerHost)
+	err := s.Run(ServerCfg.ServerHost)
 	if err != nil {
 		panic(err)
 	}
 	return nil
+}
+
+// ServerCfg is the configuration object for the server
+var ServerCfg cfg.BroadwayServer
+
+// ServerCmdFlags declare what flags can be passed to the `server` subcommand
+var ServerCmdFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:        "host, server-host",
+		Value:       "0.0.0.0:3000",
+		Usage:       "listen address for broadway http api server",
+		EnvVar:      "HOST",
+		Destination: &ServerCfg.ServerHost,
+	},
 }
--- main.go
+++ main.go
@@ -11,24 +11,19 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "broadway"
-	app.Usage = "Deploy distributed container systems"
-	app.Commands = []cli.Command{
-		{
-			Name:    "server",
-			Aliases: []string{"s"},
-			Usage:   "Start Broadway's HTTP API server",
-			Action:  cmd.ServerCmd,
-		},
-	}
+	app.Usage = "deploy distributed container systems"
 	app.Action = func(c *cli.Context) error {
 		fmt.Println(`Use "broadway server"`)
 		return nil
 	}
-
+	app.Commands = []cli.Command{
+		{
+			Name:    "server",
+			Aliases: []string{"s"},
+			Usage:   "start broadway's HTTP API server",
+			Action:  cmd.ServerCmd,
+			Flags:   cmd.ServerCmdFlags,
+		},
+	}
 	app.Run(os.Args)
-
-	// if err := cmd.RootCmd.Execute(); err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(-1)
-	// }
 }
